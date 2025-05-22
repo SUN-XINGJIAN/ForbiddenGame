accbc7e4435e3aea1c2fa8a21a19af337831e958
Sun Xingjian <xingjian.sun@ucdconnect.ie>
2025-05-22 20:37:01 +0800

    完成水位线的上涨逻辑

---
+++ src/java/board/WaterMeter.java
@@ -0,0 +1,37 @@
+package board;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class WaterMeter extends Canvas {
+    private String pictureName;
+    public int stage;
+    public WaterMeter(int number) {
+        super(105,315);
+        this.setLayoutX(541);
+        this.setLayoutY(194);
+        pictureName = "/image/WaterMeter/" + number + ".png";
+        if(number <= 2 && number >= 0){
+            stage = 1;
+        }else if(number <= 5 && number >= 3){
+            stage = 2;
+        }else if(number <= 9 && number >= 6){
+            stage = 3;
+        }
+    }
+
+    public void draw(){
+
+        GraphicsContext gc = getGraphicsContext2D();
+        gc.clearRect(0, 0, getWidth(), getHeight());
+        gc.drawImage(new Image(getClass().getResourceAsStream(pictureName)), 0, 0, getWidth(), getHeight());
+    }
+
+    public int getStage(){
+        return stage;
+    }
+
+
+
+}
