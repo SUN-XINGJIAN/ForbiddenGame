46950e0820a5a73ae92633fb3fdc613354f269e5
qly0829 <luyue.qi@ucdconnect.ie>
2025-05-26 11:43:25 +0800

    上传pilot特殊功能

--- src/java/logic/ForbiddenGameStarted.java
+++ src/java/logic/ForbiddenGameStarted.java
@@ -377,15 +377,14 @@ public class ForbiddenGameStarted {
                             (Math.abs(targetX - currX) == tileSize && targetY == currY) ||
                                     (Math.abs(targetY - currY) == tileSize && targetX == currX);
 
-                    // 处理所有玩家的基础移动
+                    // 处理所有玩家的基础移动和Pilot的特殊飞行
                     if (isAdjacent) {
                         executeMove(tile);
-                    }
-                    // 处理Pilot的特殊飞行
-                    else if (currentPlayer instanceof Pilot pilot) {
-                        if (!pilot.isSpecialFlightUsed()) { // 新增状态检查方法
-                            pilot.useSpecialFlight();
+                    } else if (currentPlayer instanceof Pilot pilot) {
+                        if (!pilot.isSpecialFlightUsed()) {
                             executeMove(tile);
+                            pilot.setSpecialFlightUsed(true);
+                            ((Pilot) currentPlayer1).setSpecialFlightUsed(true);
                         } else {
                             showMessage("Special flight already used this turn!");
                         }
@@ -459,13 +458,7 @@ public class ForbiddenGameStarted {
         turnManage.useStep();
         turnManage.showRemainSteps();
         step = turnManage.getStep();
-        if (currentPlayer instanceof Pilot) {
-            Pilot pilot = (Pilot) currentPlayer;
-            if (pilot.useSpecialFlight()) {
-                // 同步 UI 实例状态
-                ((Pilot) currentPlayer1).syncState(pilot);
-            }
-        }
+
         if (step == 0) {
             mainBoard.getChildren().remove(currentPlayer1);
 
